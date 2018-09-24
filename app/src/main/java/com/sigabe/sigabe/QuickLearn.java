4e59b2aa3b0899fbc79bed7a3a1924435cf23461
rizkarahayu <rizkarahayu42@gmail.com>
2018-09-24 09:17:42 +0700

    first

---
+++ app/src/main/java/com/sigabe/sigabe/QuickLearn.java
@@ -0,0 +1,85 @@
+package com.sigabe.sigabe;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+public class QuickLearn extends AppCompatActivity {
+
+    private Context mContext;
+    private Activity mActivity;
+
+    private RelativeLayout mRelativeLayout;
+    private Button mButton;
+    private LinearLayout mLinearLayout;
+    private ImageButton mButtona;
+
+    private PopupWindow mPopupWindow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick_learn);
+
+        // Get the application context
+        mContext = getApplicationContext();
+
+        // Get the activity
+        mActivity = QuickLearn.this;
+
+        // Get the widgets reference from XML layout
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.rl);
+//        mLinearLayout = (LinearLayout) findViewById(R.id.lr1);
+//        mButtona = (ImageButton) findViewById(R.id.btn_audio);
+        mButton = (Button) findViewById(R.id.btn);
+
+        // Set a click listener for the text view
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Initialize a new instance of LayoutInflater service
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+                // Inflate the custom layout/view
+                View customView = inflater.inflate(R.layout.activity_pop_up,null);
+
+                // Initialize a new instance of popup window
+                mPopupWindow = new PopupWindow(
+                        customView,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT
+                );
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+
+                // Get a reference for the custom view close button
+                ImageButton closeButton = (ImageButton) customView.findViewById(R.id.ib_close);
+
+                // Set a click listener for the popup window close button
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Dismiss the popup window
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                // Finally, show the popup window at the center location of root relative layout
+                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
+            }
+        });
+    }
+}
